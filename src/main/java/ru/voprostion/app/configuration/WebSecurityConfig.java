e725518e435c0809b1cd6dcd22e00e782b094bdd
woainikk <woainimrgrt@gmail.com>
2018-05-09 21:52:59 +0300

    made registration page

--- src/main/java/ru/voprostion/app/configuration/WebSecurityConfig.java
+++ src/main/java/ru/voprostion/app/configuration/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // @formatter:off
         http
                 .authorizeRequests()
-                    .antMatchers("/resources/**", "/registration").permitAll()
+                    .antMatchers("/css/**", "/registration").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
