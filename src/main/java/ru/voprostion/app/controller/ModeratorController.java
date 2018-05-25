7df5b29008517611bf8998e967264034a034a3ae
AntipovAndrey <antipov11.11@gmail.com>
2018-05-25 23:16:06 +0300

    moderator : redirect back to the question after answer has been deleted, fixed bug with recreation of a question instead of updating

--- src/main/java/ru/voprostion/app/controller/ModeratorController.java
+++ src/main/java/ru/voprostion/app/controller/ModeratorController.java
@@ -2,25 +2,65 @@ package ru.voprostion.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.voprostion.app.domain.dto.QuestionDto;
+import ru.voprostion.app.domain.model.Question;
+import ru.voprostion.app.domain.model.Tag;
 import ru.voprostion.app.domain.usecase.DeleteAnswerUseCase;
+import ru.voprostion.app.domain.usecase.EditTagsUseCase;
+import ru.voprostion.app.domain.usecase.QuestionDetailsUseCase;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Controller
-@RequestMapping("/moderator")
 public class ModeratorController {
 
     private DeleteAnswerUseCase deleteAnswerUseCase;
+    private QuestionDetailsUseCase questionDetailsUseCase;
+    private EditTagsUseCase editTagsUseCase;
 
     @Autowired
-    public ModeratorController(DeleteAnswerUseCase deleteAnswerUseCase) {
+    public ModeratorController(DeleteAnswerUseCase deleteAnswerUseCase,
+                               QuestionDetailsUseCase questionDetailsUseCase,
+                               EditTagsUseCase editTagsUseCase) {
         this.deleteAnswerUseCase = deleteAnswerUseCase;
+        this.questionDetailsUseCase = questionDetailsUseCase;
+        this.editTagsUseCase = editTagsUseCase;
     }
 
-    @GetMapping("/answer/delete/{answerId}")
-    public String deleteAnswer(@PathVariable("answerId") Long answerId) {
+    @GetMapping("/moderator/answer/delete/{answerId}")
+    public String deleteAnswer(@PathVariable("answerId") Long answerId, HttpServletRequest request) {
         deleteAnswerUseCase.delete(answerId);
-        return "redirect:/";
+        return "redirect:" + request.getHeader("Referer");
+    }
+
+    @GetMapping("/question/{questionId}/edit")
+    public String editQuestionForm(@PathVariable("questionId") Long questionId, Model model) {
+        final QuestionDto dto = new QuestionDto();
+        dto.setId(questionId);
+        final Question question = questionDetailsUseCase.getDetailed(questionId);
+        dto.setQuestion(question.getQuestionTitle());
+        final String tagString = question.getTags()
+                .stream()
+                .map(Tag::getTagName)
+                .collect(Collectors.joining(","));
+        dto.setTags(tagString);
+        model.addAttribute("question", dto);
+        return "edit_tags";
+    }
+
+    @PostMapping("/question/{questionId}/edit")
+    public String editQuestion(@PathVariable("questionId") Long questionId,
+                               @Valid @ModelAttribute("question") QuestionDto questionDto) {
+        editTagsUseCase.setNewTags(questionId,
+                Stream.of(questionDto.getTags().split(",")).collect(Collectors.toList()));
+        return "redirect:/question/" + questionId;
     }
 }
--- src/main/java/ru/voprostion/app/domain/usecase/EditTagsUseCase.java
+++ src/main/java/ru/voprostion/app/domain/usecase/EditTagsUseCase.java
@@ -1,7 +1,5 @@
 package ru.voprostion.app.domain.usecase;
 
-import ru.voprostion.app.domain.dto.QuestionDto;
-import ru.voprostion.app.domain.dto.TagDto;
 import ru.voprostion.app.domain.model.Question;
 
 import java.util.List;
